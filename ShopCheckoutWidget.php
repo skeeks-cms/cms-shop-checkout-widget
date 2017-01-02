7f2fea203c735b13b846918a54ea36d07313af76
Саша <semenov@skeeks.com>
2017-01-02 19:06:42 +0300

    Оформление заказа

--- ShopCheckoutWidget.php
+++ ShopCheckoutWidget.php
@@ -7,10 +7,14 @@
  */
 namespace skeeks\cms\shopCheckout;
 
+use skeeks\cms\components\Cms;
 use skeeks\cms\helpers\RequestResponse;
+use skeeks\cms\models\CmsUser;
+use skeeks\cms\models\forms\SignupForm;
 use skeeks\cms\shop\models\ShopBuyer;
 use skeeks\cms\shop\models\ShopFuser;
 use skeeks\cms\shop\models\ShopOrder;
+use skeeks\cms\shop\models\ShopPersonTypeProperty;
 use yii\base\Exception;
 use yii\base\Widget;
 use yii\helpers\ArrayHelper;
@@ -85,6 +89,7 @@ class ShopCheckoutWidget extends Widget
         $rr = new RequestResponse();
         $errors = [];
 
+        //Установка присланных данных текущему покупателю
         if ($post = \Yii::$app->request->post())
         {
             $this->shopFuser->load($post);
@@ -94,8 +99,10 @@ class ShopCheckoutWidget extends Widget
             }
         }
 
+        //Создание покупателя в зависимости от выбранного типа
         $this->shopBuyer = $this->shopFuser->personType->createModelShopBuyer();
 
+        //Установка данных покупателя
         $shopBuyer = $this->shopBuyer;
         if ($shopBuyer)
         {
@@ -108,34 +115,61 @@ class ShopCheckoutWidget extends Widget
 
         if ($rr->isRequestPjaxPost())
         {
+            //Если это не просто перестроение формы, то запускается процесс создания заказа
             if (!\Yii::$app->request->post($this->notSubmitParam))
             {
                 if ($this->shopFuser->validate() && $this->shopBuyer->validate() && $this->shopBuyer->relatedPropertiesModel->validate())
                 {
-                    if ($this->shopBuyer->isNewRecord)
+                    //Сохранение покупателя
+                    $buyer = $this->shopBuyer;
+                    if ($buyer->isNewRecord)
                     {
-                        if (!$this->shopBuyer->save())
+                        if ($buyerName = $this->getBuyerName())
+                        {
+                            $buyer->name = $buyerName;
+                        }
+
+                        if (!$buyer->save())
                         {
                             throw new Exception('Not save buyer');
                         }
                     }
 
+                    //Сохранение данных покупателя
                     if (!$this->shopBuyer->relatedPropertiesModel->save())
                     {
                         throw new Exception('Not save buyer data');
                     }
 
+                    //Текущий профиль покупателя присваивается текущей корзине
                     $this->shopFuser->buyer_id = $this->shopBuyer->id;
 
-                    $newOrder = ShopOrder::createOrderByFuser($this->shopFuser);
-                    $orderUrl = $newOrder->publicUrl;
-                    $this->view->registerJs(<<<JS
+                    try
+                    {
+                        $user = $this->createUser();
+                        if ($user)
+                        {
+                            $this->shopFuser->user_id = $user->id;
+                            $this->shopFuser->save();
+                        }
+
+                        $newOrder = ShopOrder::createOrderByFuser($this->shopFuser);
+                        $orderUrl = $newOrder->publicUrl;
+
+                        $this->view->registerJs(<<<JS
 location.href='{$orderUrl}';
 JS
 );
+                    } catch (\Exception $e)
+                    {
+                        print_r("Ошибка: " . $e->getMessage());
+                    }
+
+
 
                 } else
                 {
+                    print_r("Ошибка 1");
                     /*print_r($this->shopFuser->firstErrors);
                     print_r($this->shopBuyer->firstErrors);
                     print_r($this->shopBuyer->relatedPropertiesModel->firstErrors);*/
@@ -146,6 +180,132 @@ JS
         return $this->render($this->viewFile);
     }
 
+
+    /**
+     * @return string
+     */
+    public function getBuyerName()
+    {
+        $rp = $this->shopBuyer->relatedPropertiesModel;
+        $modelBuyerName = null;
+
+        foreach ($rp->toArray() as $code => $value)
+        {
+            /**
+             * @var $property ShopPersonTypeProperty
+             */
+            $property = $rp->getRelatedProperty($code);
+            if ($property->is_buyer_name == Cms::BOOL_Y)
+            {
+                $modelBuyerName[] = $value;
+            }
+        }
+
+        return $modelBuyerName ? implode(", ", $modelBuyerName) : $this->shopBuyer->shopPersonType->name . " (" . \Yii::$app->formatter->asDate(time(), 'medium') . ")";
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        $rp         = $this->shopBuyer->relatedPropertiesModel;
+        $email      = "";
+
+        foreach ($rp->toArray() as $code => $value)
+        {
+            /**
+             * @var $property ShopPersonTypeProperty
+             */
+            $property = $rp->getRelatedProperty($code);
+            if ($property->is_user_email == Cms::BOOL_Y)
+            {
+                $email = $value;
+            }
+        }
+
+        return $email;
+
+    }
+
+    public function createUser()
+    {
+        //Пользователь уже авторизован
+        if (!\Yii::$app->user->isGuest)
+        {
+            return \Yii::$app->user->identity;
+        }
+
+        //Нужно создать пользователя
+        $userPhone = "";
+        $userUsername = "";
+        $userName = "";
+
+        $rp = $this->shopBuyer->relatedPropertiesModel;
+        //Проверка свойств
+        foreach ($rp->toArray($rp->attributes()) as $code => $value)
+        {
+            $property = $rp->getRelatedProperty($code);
+            /**
+             * @var $property ShopPersonTypeProperty
+             */
+            if ($property->is_user_name == Cms::BOOL_Y)
+            {
+                $userName = $value;
+            }
+
+            if ($property->is_user_username == Cms::BOOL_Y)
+            {
+                $userUsername = $value;
+            }
+
+            if ($property->is_user_phone == Cms::BOOL_Y)
+            {
+                $userPhone = $value;
+            }
+        }
+
+
+        $userEmail = $this->getEmail();
+        if (!$userEmail)
+        {
+            return false;
+        }
+
+        if ($userExist = CmsUser::find()->where(['email' => $userEmail])->one())
+        {
+            return false;
+        }
+
+        $newUser             = new SignupForm();
+        $newUser->scenario   = SignupForm::SCENARION_ONLYEMAIL;
+        $newUser->email      = $userEmail;
+
+        if (!$user = $newUser->signup())
+        {
+            return false;
+        }
+
+        if ($userUsername)
+        {
+            $user->username      = $userUsername;
+            $user->save();
+        }
+
+        if ($userName)
+        {
+            $user->name      = $userName;
+            $user->save();
+        }
+
+        if ($userPhone)
+        {
+            $user->phone      = $userPhone;
+            $user->save();
+        }
+
+        return $user;
+    }
 
     /**
      * @return bool
