a1f26814ebef30c2db0dd726bc084a0605a87b19
semenov <semenov@skeeks.com>
2019-01-29 23:19:33 +0300

    dev

--- messages/ru/main.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'Submit'          =>  'Отправить',
-    'Error'          =>  'Ошибка',
-    'Check the correctness of filling the form fields'          =>  'Проверьте правильность заполнения полей формы',
-];
